fe8ec97ad47fe708095b79e811385d83797d9d39
STARLORD1401 <shreyaspjoshi.14@gmail.com>
2022-02-09 16:44:21 +0530

    imported express

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-"shreyas";
+import express from "express";
+import mongoose from "mongoose";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+
+//Middlewares
+
+//DBconfig
+
+//API Endpoints
+app.get("/", (req, res) => res.status(200).send("hello clever programmers"));
+
+//Listener
+app.listen(port, () => console.log(`listening on localhost:${port} `));
